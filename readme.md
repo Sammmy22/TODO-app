980395bd235e9b5248067a9a425db3b177702ef9
Sammmy22 <sambhavc225@outlook.com>
2023-01-31 14:40:39 +0530

    localStorage implementation

--- readme.md
+++ readme.md
@@ -1,3 +1,5 @@
 # TODO App
 
-## This was real fun, I used picocss for the first time and the results are outstanding. Feel free to give any suggestions, thank you.
+## This was real fun, I used picocss for the first time and the results are outstanding I used localStorage too for the first time making it my 1st CRUD Operative App.
+
+#### Feel free to give any suggestions, thank you.
--- script.js
+++ script.js
@@ -2,22 +2,41 @@ const title = document.querySelector("#title");
 
 const desc = document.querySelector("#desc");
 
+let titles = [];
+
+const container = document.querySelector("#container");
 document.querySelector("#submit").addEventListener("click", function () {
-  const time = new Date();
-  const container = document.querySelector("#container");
+  const inputTime = new Date();
   let title_content = title.value;
-  let desciption = desc.value;
+  let description = desc.value;
+
+  const time = `${inputTime.getHours()}:${
+    inputTime.getMinutes() < 10
+      ? "0" + inputTime.getMinutes()
+      : inputTime.getMinutes()
+  } ${inputTime.getDate()}-${
+    inputTime.getMonth() + 1
+  }-${inputTime.getFullYear()}`;
+
+  const task_obj = {
+    name: title_content,
+    description: description,
+    time: time,
+  };
+
+  localStorage.setItem(title_content, JSON.stringify(task_obj));
+
+  titles.push(title_content);
+  localStorage.setItem("keys", titles);
 
   title.value = "";
   desc.value = "";
 
   container.innerHTML += `<article class="task">
         <header>${title_content}</header>
-        ${desciption}
+        ${description}
         <footer>
-          Added on: ${`${time.getHours()}:${
-            time.getMinutes() < 10 ? "0" + time.getMinutes() : time.getMinutes()
-          } ${time.getDate()}-${time.getMonth() + 1}-${time.getFullYear()}`} 
+          Added on: ${time} 
           <button class="delete">Remove</button>
         </footer>
       </article>`;
@@ -29,6 +48,56 @@ document.querySelector("#submit").addEventListener("click", function () {
   for (let i = 0; i < remove.length; i++) {
     remove[i].addEventListener("click", function () {
       tasks[i].remove();
+      const removed = this.parentNode.parentNode.childNodes[1].innerHTML;
+
+      for (let i = 0; i < titles.length; i++) {
+        if (titles[i] == removed) {
+          titles.splice(i, 1);
+        }
+      }
+      localStorage.setItem("keys", titles);
+      localStorage.removeItem(removed);
+    });
+  }
+});
+
+const body = document.querySelector("body");
+
+window.addEventListener("load", function () {
+  // alert("Document loaded");
+  const prevs = localStorage.getItem("keys").split(",");
+
+  if (prevs.length === 0) {
+  }
+
+  for (let i = 0; i < prevs.length; i++) {
+    const task_obj = JSON.parse(localStorage.getItem(prevs[i]));
+    // console.log(task_obj);
+    container.innerHTML += `<article class="task">
+        <header>${task_obj.name}</header>
+        ${task_obj.description}
+        <footer>
+          Added on: ${task_obj.time} 
+          <button class="delete">Remove</button>
+        </footer>
+      </article>`;
+  }
+  const tasks = document.querySelectorAll(".task");
+
+  const remove = document.querySelectorAll(".delete");
+
+  for (let i = 0; i < remove.length; i++) {
+    remove[i].addEventListener("click", function () {
+      tasks[i].remove();
+      const removed = this.parentNode.parentNode.childNodes[1].innerHTML;
+
+      for (let i = 0; i < titles.length; i++) {
+        if (titles[i] == removed) {
+          titles.splice(i, 1);
+        }
+      }
+      localStorage.setItem("keys", titles);
+      localStorage.removeItem(removed);
     });
   }
 });
